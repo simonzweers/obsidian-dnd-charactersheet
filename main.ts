8cbfd2221cbe976d2c3af05e192d0be81f83392d
Simon Zweers <simon.zweers@protonmail.com>
2026-08-24 16:24:23 +0200

    Fix obsidian guidelines in main.ts

--- main.ts
+++ main.ts
@@ -27,7 +27,7 @@ export default class CharacterSheet extends Plugin {
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new CharacterSheetModal(this.app).open();
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
@@ -50,7 +50,7 @@ export default class CharacterSheet extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new CharacterSheetModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -61,15 +61,6 @@ export default class CharacterSheet extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 
 		this.registerView(
 			VIEW_TYPE_CHARACTERSHEET,
@@ -117,7 +108,7 @@ export default class CharacterSheet extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class CharacterSheetModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
@@ -133,28 +124,28 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: CharacterSheet;
-
-	constructor(app: App, plugin: CharacterSheet) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+// class SampleSettingTab extends PluginSettingTab {
+// 	plugin: CharacterSheet;
+//
+// 	constructor(app: App, plugin: CharacterSheet) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
+//
+// 	display(): void {
+// 		const {containerEl} = this;
+//
+// 		containerEl.empty();
+//
+// 		new Setting(containerEl)
+// 			.setName('Setting #1')
+// 			.setDesc('This will do something')
+// 			.addText(text => text
+// 				.setPlaceholder('Enter your secret')
+// 				.setValue(this.plugin.settings.mySetting)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.mySetting = value;
+// 					await this.plugin.saveSettings();
+// 				}));
+// 	}
+// }
